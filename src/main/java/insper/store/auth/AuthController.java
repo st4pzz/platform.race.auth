7e638f77aa6ae9d6407e1dd7c8d34f52bb04edda
Humberto Sandmann <hsandmann@ieee.org>
2024-05-08 05:37:26 -0300

    k8s

--- src/main/java/insper/store/auth/AuthController.java
+++ src/main/java/insper/store/auth/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "store-auth")
+@FeignClient(name = "auth")
 public interface AuthController {
 
     @PostMapping("/auth/register")
